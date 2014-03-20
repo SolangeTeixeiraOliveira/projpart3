d0d1ed4fad7e716be61076550e431552eaba42ce
Ryanna Yang <imryanna@gmail.com>
2014-03-20 10:53:45 -0700

    Added Borrower Buttons
    I am thinking we might need to change how our UI would look since I
    don’t think we would want to give all the options to every single
    person (clerk - will see the options for borrower and librarian…) :S

--- src/LibraryWindow.java
+++ src/LibraryWindow.java
@@ -12,11 +12,11 @@ public class LibraryWindow {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
+        JFrame frame = new JFrame("Welcome to the Library");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel1 = new JPanel();
-        panel1.setBackground(Color.GREEN);
+        panel1.setBackground(Color.DARK_GRAY);
         panel1.setPreferredSize(new Dimension(200, 500));
         
         JButton button1 = new JButton("Button 1");
@@ -25,10 +25,23 @@ public class LibraryWindow {
         panel1.add(button2, BorderLayout.CENTER);
         
         JPanel panel2 = new JPanel();
-        panel2.setBackground(Color.MAGENTA);
+        panel2.setBackground(Color.GRAY);
         panel2.setPreferredSize(new Dimension(200, 500));
+        
+        JButton borrower = new JButton("Borrower");
+        panel2.add(borrower, BorderLayout.CENTER);
+        
+        JButton borrowerbutton1 = new JButton("Search Library");
+        panel2.add (borrowerbutton1, BorderLayout.CENTER);
+        JButton borrowerbutton2 = new JButton("Check Account");
+        panel2.add (borrowerbutton2, BorderLayout.CENTER);
+        JButton borrowerbutton3 = new JButton("Hold Request");
+        panel2.add (borrowerbutton3, BorderLayout.CENTER);
+        JButton borrowerbutton4 = new JButton("Pay Fine");
+        panel2.add (borrowerbutton4, BorderLayout.CENTER);
+        
         JPanel panel3 = new JPanel();
-        panel3.setBackground(Color.YELLOW);
+        panel3.setBackground(Color.LIGHT_GRAY);
         panel3.setPreferredSize(new Dimension(200, 500));
         frame.add(panel1, BorderLayout.LINE_START);
         frame.add(panel2, BorderLayout.CENTER);
