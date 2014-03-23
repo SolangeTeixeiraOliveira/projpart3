ef2e3ad8e408a19b8e3b779f1fe8c51e774d84d4
FrancesKR <franceskrussell@gmail.com>
2014-03-22 22:56:50 -0700

    Added clerk buttons, got everything centered

--- src/LibraryWindow.java
+++ src/LibraryWindow.java
@@ -1,10 +1,15 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.*;        
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 public class LibraryWindow {
+	
     /**
      * Create the GUI and show it.  For thread safety,
      * this method should be invoked from the
@@ -15,34 +20,49 @@ public class LibraryWindow {
         JFrame frame = new JFrame("Welcome to the Library");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Clerk panel
         JPanel panel1 = new JPanel();
-        panel1.setBackground(Color.DARK_GRAY);
-        panel1.setPreferredSize(new Dimension(200, 500));
+        panel1.setBackground(Color.LIGHT_GRAY);
+        panel1.setPreferredSize(new Dimension(200, 300));
         
-        JButton button1 = new JButton("Button 1");
-        panel1.add(button1, BorderLayout.CENTER);
-        JButton button2 = new JButton("Button 2");
-        panel1.add(button2, BorderLayout.CENTER);
+        JLabel clerklabel = new JLabel("Clerk", SwingConstants.CENTER);
+        clerklabel.setPreferredSize(new Dimension(130, 30));
+        panel1.add(clerklabel);
+        JButton addBorrowerBtn = new JButton("Add Borrower");
+        addBorrowerBtn.setPreferredSize(new Dimension(130, 30));
+        panel1.add(addBorrowerBtn);
+        JButton checkOutBtn = new JButton("Check Out Items");
+        checkOutBtn.setPreferredSize(new Dimension(130, 30));
+        panel1.add(checkOutBtn);
+        JButton returnItemsBtn = new JButton("Return Items");
+        returnItemsBtn.setPreferredSize(new Dimension(130, 30));
+        panel1.add(returnItemsBtn);
         
+        // Borrower panel
         JPanel panel2 = new JPanel();
         panel2.setBackground(Color.GRAY);
-        panel2.setPreferredSize(new Dimension(200, 500));
+        panel2.setPreferredSize(new Dimension(200, 300));
         
-        JButton borrower = new JButton("Borrower");
-        panel2.add(borrower, BorderLayout.CENTER);
-        
+        JLabel borrower = new JLabel("Borrower", SwingConstants.CENTER);
+        borrower.setPreferredSize(new Dimension(130, 30));
+        panel2.add(borrower);
         JButton borrowerbutton1 = new JButton("Search Library");
-        panel2.add (borrowerbutton1, BorderLayout.CENTER);
+        borrowerbutton1.setPreferredSize(new Dimension(130, 30));
+        panel2.add (borrowerbutton1);
         JButton borrowerbutton2 = new JButton("Check Account");
-        panel2.add (borrowerbutton2, BorderLayout.CENTER);
+        borrowerbutton2.setPreferredSize(new Dimension(130, 30));
+        panel2.add (borrowerbutton2);
         JButton borrowerbutton3 = new JButton("Hold Request");
-        panel2.add (borrowerbutton3, BorderLayout.CENTER);
+        borrowerbutton3.setPreferredSize(new Dimension(130, 30));
+        panel2.add (borrowerbutton3);
         JButton borrowerbutton4 = new JButton("Pay Fine");
-        panel2.add (borrowerbutton4, BorderLayout.CENTER);
+        borrowerbutton4.setPreferredSize(new Dimension(130, 30));
+        panel2.add (borrowerbutton4);
         
+        // Librarian panel
         JPanel panel3 = new JPanel();
         panel3.setBackground(Color.LIGHT_GRAY);
-        panel3.setPreferredSize(new Dimension(200, 500));
+        panel3.setPreferredSize(new Dimension(200, 300));
         frame.add(panel1, BorderLayout.LINE_START);
         frame.add(panel2, BorderLayout.CENTER);
         frame.add(panel3, BorderLayout.LINE_END);
