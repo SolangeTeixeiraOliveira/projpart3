edf5684e06f6fa1b137d7e6e43b52c4a1735ee56
Sol <oliveirastbr@gmail.com>
2014-03-31 23:43:58 -0700

    Connector

--- src/sqlFunctions/Connector.java
+++ src/sqlFunctions/Connector.java
@@ -17,7 +17,12 @@ public class Connector {
 						.registerDriver(new oracle.jdbc.driver.OracleDriver());
 				con = DriverManager.getConnection(
 
-				"jdbc:oracle:thin:@localhost:1522:ug", "ora_x4q7", "a45775103");
+
+				//"jdbc:oracle:thin:@localhost:1522:ug", "ora_y2c9", "a64353139");
+				"jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "ora_y2c9", "a64353139");
+
+//				"jdbc:oracle:thin:@localhost:1522:ug", "ora_x4q7", "a45775103");
+
 
 				con.setAutoCommit(false);
 			} catch (SQLException e) {
